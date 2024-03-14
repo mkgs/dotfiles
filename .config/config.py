d22e56736d5b2c498bffb25cfd24218b6615d852
Michael Gisi <gisi@umich.edu>
2024-03-14 13:41:52 -0400

    add qutebrowser config

---
+++ .config/config.py
@@ -0,0 +1,9 @@
+config.load_autoconfig()
+c.fonts.default_family = ["Cascadia Code"]
+c.fonts.default_size = '10pt'
+c.statusbar.position = 'bottom'
+c.statusbar.show = 'always'
+c.statusbar.widgets = ['keypress', 'search_match', 'url', 'scroll', 'history', 'tabs', 'progress']
+c.tabs.position = 'top'
+c.tabs.show = 'multiple'
+config.source('nord-qutebrowser.py')
